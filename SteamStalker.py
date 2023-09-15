f802c4d081ffc4ca063a637e63bbb58c447e4b0b
HoroTW <horo@horotw.com>
2023-09-15 08:27:17 +0200

    Add `dotenv`

--- SteamStalker.py
+++ SteamStalker.py
@@ -5,5 +5,8 @@ Small script to check if steam friends are playing a game and send a
 notification using a telegram bot.
 """
 
+# load the dot env file
+from dotenv import load_dotenv
+load_dotenv()
 
 
